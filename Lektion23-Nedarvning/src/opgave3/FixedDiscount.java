9866ea8f32526a21c1122bef08140fdf557b248b
Torben Grove <torben_grove@live.dk>
2018-04-13 09:32:10 +0200

    Lektion23 - opgave3 helt done!

--- Lektion23-Nedarvning/src/opgave3/FixedDiscount.java
+++ Lektion23-Nedarvning/src/opgave3/FixedDiscount.java
@@ -3,13 +3,13 @@ package opgave3;
 public class FixedDiscount extends Discount {
 	double amount;
 
-	public FixedDiscount(double originalPrice, double amount) {
+	public FixedDiscount(double originalPrice) {
 		// TODO Auto-generated constructor stub
 		super(originalPrice);
-		this.amount = amount;
+		this.amount = 0;
 	}
 
-	public void setAmount(double amount) {
+	private void setAmount(double amount) {
 		this.amount = amount;
 	}
 
@@ -20,6 +20,11 @@ public class FixedDiscount extends Discount {
 	@Override
 	public double getDiscountedPrice(double originalPrice) {
 		// TODO Auto-generated method stub
+		if (originalPrice > 1000) {
+			setAmount(250);
+		} else {
+			setAmount(0);
+		}
 		return super.getDiscountedPrice(originalPrice) - amount;
 	}
 }
--- Lektion23-Nedarvning/src/opgave3/OrderApp.java
+++ Lektion23-Nedarvning/src/opgave3/OrderApp.java
@@ -4,19 +4,18 @@ import java.time.LocalDate;
 
 public class OrderApp {
 	public static void main(String[] args) {
-		Product product1 = new Product(1, "Transformer", 10);
-		Product product2 = new Product(2, "Morten From", 10);
-		Product product3 = new Product(3, "Benjamin Hauge", 10);
-		Product product4 = new Product(4, "Doktor Hansen", 10);
-		Product product5 = new Product(5, "Jan Børkop", 10);
+		Product product1 = new Product(1, "Transformer", 50);
+		Product product2 = new Product(2, "Morten From", 140);
+		Product product3 = new Product(3, "Benjamin Hauge", 60);
+		Product product4 = new Product(4, "Doktor Hansen", 1200);
+		Product product5 = new Product(5, "Jan Børkop", 100);
 
 		Customer customer1 = new Customer("Henrik", LocalDate.parse("1996-01-01"));
 		Customer customer2 = new Customer("Martin", LocalDate.parse("1984-01-01"));
 
 		Order order1 = new Order(1);
 		Order order2 = new Order(2);
-		Order order3 = new Order(3);
-		Order order4 = new Order(4);
+
 		order1.createOrderLine(1, product1);
 		order1.createOrderLine(1, product2);
 		order1.createOrderLine(1, product3);
@@ -31,12 +30,12 @@ public class OrderApp {
 		// customer2.addOrder(order4);
 		System.out.println(order1.getOrderPrice());
 
-		PercentDiscount percentDiscount = new PercentDiscount(order1.getOrderPrice(), 50);
-		FixedDiscount discount = new FixedDiscount(order1.getOrderPrice(), 10);
-		customer1.setDiscount(discount);
+		Discount percentDiscount = new PercentDiscount(order1.getOrderPrice());
+		Discount fixedDiscount = new FixedDiscount(order1.getOrderPrice());
+		customer1.setDiscount(fixedDiscount);
 		customer2.setDiscount(percentDiscount);
 		System.out.println(customer1.getDiscountedPrice(order1));
-		System.out.println(customer2.getDiscountedPrice(order2));
+		System.out.println(customer2.getDiscountedPrice(order1));
 
 	}
 }
